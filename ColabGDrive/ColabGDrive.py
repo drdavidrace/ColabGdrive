e11460ad2fa8923be12eab64df3844480898e1ab
drdavidrace <41238989+drdavidrace@users.noreply.github.com>
2018-08-07 13:17:17 -0500

    9

--- ColabGDrive/ColabGDrive.py
+++ ColabGDrive/ColabGDrive.py
@@ -1,28 +1,13 @@
 import os, sys, re
-import subprocess
-import importlib
-del(pip)
-
+import pydrive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+      
 class ColabGDrive:
   
   def __init__(self, current_dir = 'root'):
-    try:
-      subprocess(['python','-m','pip','install','-U -q','PyDrive'])
-      import pydrive
-      from pydrive.auth import GoogleAuth
-      from pydrive.drive import GoogleDrive
-      from google.colab import auth
-      from oauth2client.client import GoogleCredentials
-
-    except:
-      #print('pydrive is not installed')
-      subprocess.call(['python','-m','pip', 'install -U -q','PyDrive'])
-      import pydrive
-      from pydrive.auth import GoogleAuth
-      from pydrive.drive import GoogleDrive
-      from google.colab import auth
-      from oauth2client.client import GoogleCredentials
-      print("PyDrive installed and imported")
     auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
