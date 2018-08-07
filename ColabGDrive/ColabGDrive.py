f3d8a0506cb056498a67550bb28860f204ea5312
drdavidrace <41238989+drdavidrace@users.noreply.github.com>
2018-08-07 12:10:46 -0500

    Building ColabDrive Object

--- ColabGDrive/ColabGDrive.py
+++ ColabGDrive/ColabGDrive.py
@@ -1,1 +1,20 @@
+import os, sys, re
+import pip
 
+Class ColabGDrive:
+  
+  __init__(current_dir = 'root'):
+    try:
+      import pydrive
+      from pydrive.auth import GoogleAuth
+      from pydrive.drive import GoogleDrive
+      from google.colab import auth
+      from oauth2client.client import GoogleCredentials
+    except:
+      pip.main(['install','-U -q','PyDrive'])
+      import pydrive
+      from pydrive.auth import GoogleAuth
+      from pydrive.drive import GoogleDrive
+      from google.colab import auth
+      from oauth2client.client import GoogleCredentials
+      
