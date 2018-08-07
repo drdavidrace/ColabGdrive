67d1b05c83829a8b193cde54c77d798da5492cfa
drdavidrace <41238989+drdavidrace@users.noreply.github.com>
2018-08-07 14:25:04 -0500

    14

--- ColabGDrive/ColabGDrive.py
+++ ColabGDrive/ColabGDrive.py
@@ -1,14 +1,21 @@
 import os, sys, re
+    
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
 
 class ColabGDrive:
   #
   def __init__(self, current_dir = 'root'):
-    from pydrive.auth import GoogleAuth
-    from pydrive.drive import GoogleDrive
-    from google.colab import auth
-    from oauth2client.client import GoogleCredentials
+    #
     print("Entering Initialization")
+    self.myGdrive = []
+  
+  def connect_drive(self, cur_dir = 'root'):
+    #
     auth.authenticate_user()
     gauth = GoogleAuth()
     gauth.credentials = GoogleCredentials.get_application_default()
-    self.myGdrive = GoogleDrive(gauth)
+    self.myGDrive = GoogleDrive(gauth)
+    
