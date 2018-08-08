575fda5107f32b5283110dd0c5590db906a73a16
dr.david.race <dr.david.race@gmail.com>
2018-08-08 17:47:01 -0400

    file management 2

--- ColabGDrive/ColabGDrive.py
+++ ColabGDrive/ColabGDrive.py
@@ -53,7 +53,45 @@ class ColabGDrive:
   def get_file_information(self,file_name = ''):
     if(len(file_name) == 0):
       return None
-    return list_file_dict(self,file_name)
+    return self.myGDrive.__list_file_dict_(self,file_name)
     
     
   #change directory
+  
+  #helpers
+  def __list_file_dict_(drive = None, inStr = ''):
+  '''Returns a dictionary with the file name and file ID (if exists) - None otherwise'''
+  if (drive is None):
+    return None
+
+  wStr = clean_directory_path(inStr)
+  inStruct = wStr.split('/')
+  if (not (inStruct[0] == "root")):
+    inStruct = ["root"] + inStruct
+  fileID = None
+  fileResult = None
+  for i in range(1,len(inStruct)):
+    if(i == 1):
+      file_list = drive.ListFile({'q': "'root' in parents and title = '{:s}' and trashed=false".format(inStruct[1])}).GetList()
+      if(not file_list):
+        fileID = None
+        break
+      else:
+        fileID = file_list[0]['id']
+        if(i == len(inStruct) - 1):
+          fileName = inStruct[i]
+          fileResult = {"title" : fileName, "id":  fileID}
+          break
+    else:
+      file_list = drive.ListFile({'q': "title = '{:s}' and '{:s}' in parents and trashed=false".format(inStruct[i],fileID)}).GetList()
+      if(not file_list):
+        fileID = None
+        break
+      else:
+        print(file_list[0])
+        fileID = file_list[0]['id']
+        if(i == len(inStruct) - 1):
+          fileName = inStruct[i]
+          fileResult = {"title" : fileName, "id":  fileID}
+          break
+  return(fileResult)
