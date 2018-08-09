b56a7d597751640f88d7e25b6c31333460a50544
dr.david.race <dr.david.race@gmail.com>
2018-08-09 14:26:21 -0400

    ls with relative path support

--- test/shell/test_ColabGDrive.py
+++ test/shell/test_ColabGDrive.py
@@ -1,30 +1,34 @@
 #!/usr/bin/env /home/cabox/anaconda3/bin/python3
-import ColabGDrive 
 
-from ColabGDrive import ColabGDrive 
+#ISSUE:  don't know how to allow access to google drive from non-colab yet
 
-myGdrive = ColabGDrive.ColabGDrive()
-myGdrive.connect_gdrive()
-if myGdrive.is_connected():
-  print(myGdrive.get_info())
-  print("CWD: {:s}".format(myGdrive.get_current_directory()))
-else:
-  print("myGdrive does not exist:-)")
-#
-#  Check testing information
-#
-print("=======Dirctory Information=======")
-print(myGdrive.ls_file('TempTest'))
-myGdrive.ls_file('TempTest',print_val=True)
-print("=====Directory Content Information=====")
-print(myGdrive.ls_file('TempTest/*'))
-myGdrive.ls_file('TempTest/*',print_val=True)
-print("=====Directory Content Information 2=====")
-print(myGdrive.ls_file('TempTest/.'))
-myGdrive.ls_file('TempTest/.',print_val=True)
-print("=====Directory Content Information 3=====")
-print(myGdrive.ls_file('TempTest/..'))
-myGdrive.ls_file('TempTest/..',print_val=True)
-print("=====Directory Content Information 4=====")
-print(myGdrive.ls_file('.'))
-myGdrive.ls_file('.',print_val=True)
+
+# import ColabGDrive 
+
+# from ColabGDrive import ColabGDrive 
+
+# myGdrive = ColabGDrive.ColabGDrive()
+# myGdrive.connect_gdrive()
+# if myGdrive.is_connected():
+#   print(myGdrive.get_info())
+#   print("CWD: {:s}".format(myGdrive.get_current_directory()))
+# else:
+#   print("myGdrive does not exist:-)")
+# #
+# #  Check testing information
+# #
+# print("=======Dirctory Information=======")
+# print(myGdrive.ls_file('TempTest'))
+# myGdrive.ls_file('TempTest',print_val=True)
+# print("=====Directory Content Information=====")
+# print(myGdrive.ls_file('TempTest/*'))
+# myGdrive.ls_file('TempTest/*',print_val=True)
+# print("=====Directory Content Information 2=====")
+# print(myGdrive.ls_file('TempTest/.'))
+# myGdrive.ls_file('TempTest/.',print_val=True)
+# print("=====Directory Content Information 3=====")
+# print(myGdrive.ls_file('TempTest/..'))
+# myGdrive.ls_file('TempTest/..',print_val=True)
+# print("=====Directory Content Information 4=====")
+# print(myGdrive.ls_file('.'))
+# myGdrive.ls_file('.',print_val=True)
