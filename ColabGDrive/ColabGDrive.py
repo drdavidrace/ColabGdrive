caa57b1eb84ea1e290a79c8d4ac7bdaeac7d4d3e
dr.david.race <dr.david.race@gmail.com>
2018-08-10 17:55:46 -0400

    Update to use logging

--- ColabGDrive/ColabGDrive.py
+++ ColabGDrive/ColabGDrive.py
@@ -188,8 +188,7 @@ class ColabGDrive:
     
     work_name = build_full_path(self, name.strip())
     if(self.Logger.isEnabledFor(logging.INFO)):
-      log_info = pformat('WORK NAME: {:s}',work_name)
-      pprint(log_info)
+      pprint('WORK NAME: {:s}',work_name)
     if(len(work_name) == 0):
     #Info Information
       if(self.Logger.isEnabledFor(logging.INFO)):
