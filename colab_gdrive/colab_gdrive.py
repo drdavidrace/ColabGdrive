9ddf6611c3973ece1e475336f15a9a4c32b3c1de
dr.david.race <dr.david.race@gmail.com>
2018-08-20 19:01:47 -0400

    moves

--- colab_gdrive/colab_gdrive.py
+++ colab_gdrive/colab_gdrive.py
@@ -13,8 +13,8 @@ from oauth2client.client import GoogleCredentials
 #  Set logging level
 #
 #  TODO:  Including logging level information
-
-class ColabGDrive:
+#
+class colab_gdrive:
   '''
   The class manages the connection to a user's Google Drive.
   
@@ -24,18 +24,18 @@ class ColabGDrive:
   since the Colaboratory notebook is running in a VM.  It can only access the local VM files; therefore, the user needs to
   copy-from/copy-to between the local file system and the GDrive.  There is a great tool for setting up the connection to do the copies, but
   it requires many lines of code to make this happen.  The many lines of code detracts from the Colaboratory notebook flow; therefore, it
-  is best to have a library for the management.  This is such a library.  
-  
+  is best to have a library for the management.  This is such a library.
+
   It is modeled after python set of commands (which are used to manage the local VM drives); therefore, it should be easy to use.It
-  
+
   Special Note:
   -------------
-  It is possible to mount the GDrive into the local workspace, but that doesn't seem very "Google Ecosystem".  Since we may be using other 
+  It is possible to mount the GDrive into the local workspace, but that doesn't seem very "Google Ecosystem".  Since we may be using other
   Google Storage with other applications, this library is trying to be more "Google Ecosystem" like.Google
   
   WARNING:
   -------
-  This could be more efficient if it kept the fileID information, but it doesn't right now.  The Use Case is generally assumed to be fairly 
+  This could be more efficient if it kept the fileID information, but it doesn't right now.  The Use Case is generally assumed to be fairly
   low performance requirements
   
   Main Methods:
@@ -52,17 +52,17 @@ class ColabGDrive:
   def __init__(self, logging_level = logging.ERROR):
     '''
     The initialization routine sets up the internal information and sets up the GoogleDrive connection.The
-    
+
     Parameters
     ----------
     loggine_level : This sets the logging level for this object.  Default is ERROR
-    
+
     Returns
     -------
     False if no success
     True if successful
-    
-    TODO:  
+
+    TODO:
     (1)  Determine if I want to pass in an option for the cwd.  Unclear since a single cwd call can set this directory
     (2)  Do a better logging error message
     (3)  Convert to using os.path.join and other os.path functions for path work
@@ -124,8 +124,8 @@ class ColabGDrive:
         ret_val = t_gdrive
     else:
       ret_val = None
-      
-    if self.Logger.isEnabledFor(logging.INFO):
+    
+    if.Logger.isEnabledFor(logging.INFO):
       self.Logger.info("Leaving")
       self.Logger.info(pprint(inspect.currentframe().f_code.co_name))
     return ret_val
@@ -142,42 +142,42 @@ class ColabGDrive:
   def  is_connected(self):
     '''
     This returns True if it is connected and False otherwise.
-    
+
     Parameters:
     -----------
     None
-    
+
     Returns:
     --------
     True if connected and False otherwise
-    
+
     TODO:
     -----
     (1)  Just checks the local variable.  Probably should check the drive just incase it was timed out.
     '''
     return True if(self.myGDrive is not None) else False
-  
-  #  Basic information 
+
+  #  Basic information
   def get_info(self):
     '''
     This just returns the drive information.
-    
+
     Parameters:
     -----------
     None:
-    
+
     WARNING:  I am not sure what this entails at this point, so it is just information.WARNING
     '''
     return self.myGDrive
-  
+
   def getcwd(self):
     '''
     Just returns the current working directory
-    
+
     Parameters:
     -----------
     None
-    
+
     Returns:
     --------
     The current working directory
@@ -193,14 +193,14 @@ class ColabGDrive:
       (1)  Full path implementation - If the name begins with /, then it is assumed to be a full path.  Otherwise, a relative path.
       (2)  / simplification, means reducing multiple slashes to a single slash akin to Linux operations
       (3)  Path simplification - If the component name is ., then it removes this from the path.  If the component name is .., then it moves back a component
-      
+
     Other simplifications may be applied in the future
     Parameters
     ----------
     name : String for name to list
       If the last component is a ., then the last folder contents are listed
       If the last component is a *, then the last folder contents are listed
-    
+
     Returns
     -------
     None:  If there is a problem with the name
@@ -209,20 +209,20 @@ class ColabGDrive:
         id
         mimeType
         title
-        
+
     Raises
     ------
     None at this point
-    
+
     TODO
     (1)  Raise basic errors
-    
+
     '''
     if(self.Logger.isEnabledFor(logging.INFO)):
       self.Logger.info("Entering")
       self.Logger.info(pprint(inspect.currentframe().f_code.co_name))
     work_name = self._build_full_path_(name.strip())
-    
+
     ret_val = None
     if(len(work_name) == 0):
       ret_val = None
@@ -231,20 +231,20 @@ class ColabGDrive:
       ret_val = ls_file_dict
     if(self.Logger.isEnabledFor(logging.INFO)):
       self.Logger.info("Leaving")
-      
+
       self.Logger.info(pprint(inspect.currentframe().f_code.co_name))
     return(ret_val)
-    
+
   #Directory Management, uses a quasi cd methodology
   def chdir(self, name=''):
     '''
     This sets the current working directory (if valid directory) and returns the current working directory at the end of the method.
     If name is passed in as '', then it resets to 'root'
-    
+
     Parameters:
     -----------
     name:  The proposed name of the directory.  If begins with /, then an absolute path.  Otherwise a relative path.
-    
+
     Returns:
     --------
     The current working directory
@@ -253,7 +253,7 @@ class ColabGDrive:
       self.Logger.info("Entering")
       self.Logger.info(pprint(inspect.currentframe().f_code.co_name))
     if(len(name) == 0): name = 'root'
-      
+
     work_file_info = self.ls(name)
     if self.Logger.isEnabledFor(logging.INFO):
       self.Logger.info(pformat(work_file_info['full_name']))
@@ -309,6 +309,10 @@ class ColabGDrive:
 
     Result: a dictionary with the full name and the path array
       These are called full_path and path_array
+
+    TODO:
+    _____
+    (1)  Change to use of os.path splitting functions.  This will require an iterative process, but not bad
 
     '''
   #   work_str = clean_directory_path(in_str)
@@ -369,4 +373,3 @@ class ColabGDrive:
       return({'full_name': file_path['full_name'],'file_result':file_result})
     except:
       return(None)
-  
