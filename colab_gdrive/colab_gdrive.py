6492ee0a910b0eda8ff3a6ccdf3a6135deb025df
dr.david.race <dr.david.race@gmail.com>
2018-08-23 00:14:24 -0400

    Processing Upload

--- colab_gdrive/colab_gdrive.py
+++ colab_gdrive/colab_gdrive.py
@@ -595,8 +595,7 @@ class ColabGDrive:
             file_path.append(file_name)
       else:
         drive_file = self.my_gdrive.CreateFile({'id': '{:s}'.format('root')})
-        drive_file.FetchMetadata(fetch_all=True)
-        pprint(drive_file['fileSize'])
+        pprint(list(drive_file.FetchMetadata(fetch_all=True).keys()))
         t_dict = {'title': drive_file['title'], 'id': drive_file['id'], 'mimeType': drive_file['mimeType']}
         try:
           t_dict['fileSize'] = drive_file['fileSize']
